76a052005bacd01aae2018378458957a8cd5b01f
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-10-18 11:10:44 +0530

    Numbers and Math

---
+++ Basics/03_Numbers_and_Math.py
@@ -0,0 +1,20 @@
+# Addition
+print(4+5)
+
+#Subtraction
+print(5-4)
+
+# Multiplication
+print(5*4)
+
+# Division ( It Always Return Float )
+print(4/4)
+
+# Exponentiation
+print(4**5)
+
+# Modulo (Commonly Used to Know if the number is EVEN or ODD)
+print(5%4)
+
+# Integer Division ( It Will Return Integer )
+print(5//4)
