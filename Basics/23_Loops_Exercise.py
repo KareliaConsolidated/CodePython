2224bb2fc938c2ba644be2e988be7a099e938f12
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-10-27 08:33:27 +0530

    Loops Exercise Basic

---
+++ Basics/23_Loops_Exercise.py
@@ -0,0 +1,18 @@
+# # Way 01
+# for num in range(1,21):
+# 	if num == 4 or num == 13:
+# 		print(f"x is {num}, and its unlucky")
+# 	elif num % 2 == 0:
+# 		print(f"x is {num}, and its even")
+# 	else:
+# 		print(f"x is {num}, and its odd")
+
+# Way 02
+for num in range(1,21):
+	if num == 4 or num == 13:
+		state = "unlucky"
+	elif num % 2 == 0:
+		state = "even"
+	else:
+		state = "odd"
+	print(f"{num} is {state}")
