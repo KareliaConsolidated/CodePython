8a1c279fb8878f9aa580b3b47417070fc521530a
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-11-17 11:53:13 +0530

    Iterators Basics

---
+++ Basics/111_Iterators.py
@@ -0,0 +1,156 @@
+# Iterators vs Iterables
+# Iterable
+# Examples: Lists, Strings, Dictionaries, File Connections
+# An object with an associated iter() method.
+# Applying iter() to an iterable creates an iterator.
+# Iterator
+# Produces next value with next()
+word = 'Direct'
+it = iter(word)
+print(next(it)) # D
+print(next(it)) # i
+print(next(it)) # r
+print(next(it)) # e
+print(next(it)) # c
+print(next(it)) # t
+it = iter(word)
+print(*it) # Direct # * operator or Splat Operator
+
+#=========================================================#
+
+# Create a list of strings: flash
+flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+
+# Print each list item in flash using a for loop
+for person in flash:
+    print(person)
+
+
+# Create an iterator for flash: superhero
+superhero = iter(flash)
+
+# Print each item from the iterator
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
+
+#=========================================================#
+
+# Create an iterator for range(3): small_value
+small_value = iter(range(3))
+
+# Print the values in small_value
+print(next(small_value))
+print(next(small_value))
+print(next(small_value))
+
+# Loop over range(3) and print the values
+for num in range(3):
+    print(num)
+
+
+# Create an iterator for range(10 ** 100): googol
+googol = iter(range(10**100))
+
+# Print the first 5 values from googol
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
+
+#=========================================================#
+#====# Iterators as function arguments #====#
+
+# Create a range object: values
+values = range(10,21)
+
+# Print the range object
+print(values)
+
+# Create a list of integers: values_list
+values_list = list(values)
+
+# Print values_list
+print(values_list)
+
+# Get the sum of values: values_sum
+values_sum = sum(values)
+
+# Print values_sum
+print(values_sum)
+
+#=========================================================#
+
+# Create a list of strings: mutants
+mutants = ['charles xavier', 
+            'bobby drake', 
+            'kurt wagner', 
+            'max eisenhardt', 
+            'kitty pryde']
+
+# Create a list of tuples: mutant_list
+mutant_list = list(enumerate(mutants))
+
+# Print the list of tuples
+print(mutant_list)
+
+# Unpack and print the tuple pairs
+for index1, value1 in enumerate(mutants):
+    print(index1, value1)
+
+# Change the start index
+for index2, value2 in enumerate(mutants, start=1):
+    print(index2, value2)
+
+#=========================================================#
+
+mutants = ['charles xavier',
+ 'bobby drake',
+ 'kurt wagner',
+ 'max eisenhardt',
+ 'kitty pryde']
+
+aliases = ['prof x', 'iceman', 'nightcrawler', 'magneto', 'shadowcat']
+
+powers = ['telepathy',
+ 'thermokinesis',
+ 'teleportation',
+ 'magnetokinesis',
+ 'intangibility']
+
+ # Create a list of tuples: mutant_data
+mutant_data = list(zip(mutants, aliases, powers))
+
+# Print the list of tuples
+print(mutant_data)
+
+# Create a zip object using the three lists: mutant_zip
+mutant_zip = zip(mutants, aliases, powers)
+
+# Print the zip object
+print(mutant_zip)
+
+# Unpack the zip object and print the tuple values
+for value1, value2, value3 in mutant_zip:
+    print(value1, value2, value3)
+
+#=========================================================#
+# USING * AND ZIP to 'UNZIP'
+
+# Create a zip object from mutants and powers: z1
+z1 = zip(mutants, powers)
+
+# Print the tuples in z1 by unpacking with *
+print(*z1)
+
+# Re-create a zip object from mutants and powers: z1
+z1 = zip(mutants, powers)
+
+# 'Unzip' the tuples in z1 by unpacking with * and zip(): result1, result2
+result1, result2 = zip(*z1)
+
+# Check if unpacked tuples are equivalent to original tuples
+print(result1 == mutants)
+print(result2 == powers)
