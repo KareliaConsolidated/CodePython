3ff5ca295a2eb6780bf6ede1a4322ced88f5e1f9
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-11-29 18:32:08 +0530

    Python Ex_02

---
+++ Basics/141_Python_Ex_02.py
@@ -0,0 +1,7 @@
+# Titleize
+# Write a function called titleize which accepts a string of words and returns a new string with the first letter of every word in the string capitalized.
+def titleize(string):
+	return ' '.join([s[0].upper() + s[1:] for s in string.split(' ')])
+
+print(titleize('this is awesome'))
+print(titleize('oNly cAPITALIZe fIRST'))
