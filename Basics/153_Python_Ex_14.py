e30b60b203f69cbb274712156855f80290fcc42c
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-01 20:25:33 +0530

    Python Ex_14

---
+++ Basics/153_Python_Ex_14.py
@@ -0,0 +1,16 @@
+# Next Prime Generator
+# Write a function called next_prime, which returns a generator that will yield an unlimited number of primes, starting from first prime(2). Recall that a prime number is any whole number that has exactly two divisors: one and the number itself. The first few primes are 2, 3, 5, 7, 11......
+def next_prime():
+	num = 2
+	all_primes = set()
+	while True:
+		for prime in all_primes:
+			if num % prime == 0:
+				break
+		else:
+			all_primes.add(num)
+			yield num
+		num += num % 2 + 1
+
+primes = next_prime()
+print([next(primes) for i in range(25)]) # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
