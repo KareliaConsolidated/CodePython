ab950870bd75f9a79bf7f18512606dc51930a333
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-01 20:25:58 +0530

    Python Ex_15

---
+++ Basics/154_Python_Ex_15.py
@@ -0,0 +1,5 @@
+# Reverse String
+# Write a function called reverse string which accepts a string and returns a new string with all the characters reversed.
+def reverse_string(string):
+	return string[::-1]
+print(reverse_string('awesome')) # emosewa
