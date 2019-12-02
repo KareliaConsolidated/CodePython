ddbcde65fa8a5787b089a0ae332168f120fcf3f6
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-02 09:59:31 +0530

    Python Ex_27

---
+++ Basics/166_Python_Ex_27.py
@@ -0,0 +1,8 @@
+# nth
+# Write a function called nth, which accepts a list and a number and returns the element at whatever index is the number in the list. If the number is negative, the nth element from the end is returned.
+# You can assume that number will always be between the negative value of the list length, and the list length minus 1
+def nth(lst, ind):
+	return lst[ind]
+
+print(nth(['a','b','c','d'], 1)) # 'b'
+print(nth(['a','b','c','d'], -2)) # 'c'
