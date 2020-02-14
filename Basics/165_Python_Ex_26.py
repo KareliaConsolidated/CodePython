db3927fa90d6e68f10796ce188d21a7754c9e614
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-02-14 18:28:42 +0530

    Modified

--- Basics/165_Python_Ex_26.py
+++ Basics/165_Python_Ex_26.py
@@ -5,7 +5,7 @@ def same_frequency(num1, num2):
 	d2 = {letter:str(num2).count(letter) for letter in str(num2)}
 	
 	for key,val in d1.items():
-		if not key in d2.keys():
+		if key not in d2.keys():
 			return False
 		elif d2[key] != d1[key]:
 			return False
