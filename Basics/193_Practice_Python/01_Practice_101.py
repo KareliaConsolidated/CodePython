5b5c070924855cd44e592c828308759de63dfa8f
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-05-04 15:24:38 +0530

    Practice Python Basics 101

---
+++ Basics/193_Practice_Python/01_Practice_101.py
@@ -0,0 +1,26 @@
+# Python is Case Sensitive
+# Spacing is Important
+# Use Error Messages to help you learn.
+
+# Write an expression that calculates the average of 23, 32, 64
+print(23+32+64/3)
+
+my_population = 74728
+my_population += 4000-600
+print(my_population)
+
+# Lists - Supports Membership Operators
+# Keyword - Operation
+# "in": Evaluates if object on left side is included in object on right side.
+# "not in": Evaluates if object on left side is not included in object on right side.
+
+new_str = "\n".join(["fore", "aft", "starboard", "port"])
+print(new_str)
+
+# A set is a data type for mutable unordered collections of unique elements. One application of a set is to quickly remove duplicates from a list.
+
+# Identity Operators
+# Keyword	Operator
+# is	evaluates if both sides have the same identity
+# is not	evaluates if both sides have different identities
+# You can check if a key returned None with the is operator. You can check for the opposite using is not.
